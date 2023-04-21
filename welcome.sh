1e147f442139303836315ecd40fc5c30e50190b4
root <root@final-project.c.teehee-375116.internal>
2023-04-21 02:40:42 +0000

    Edited welcome.sh

---
+++ welcome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Welcome to the bash command tutorial!"
+echo "The purpose of this program is to teach you how to use basic bash commands in your terminal."
+
+echo "You have the option to complete the entire tutorial at once, or pick a specific command to learn about."
+
+echo "What would you like to do?"
+echo "1. Full tutorial"
+echo "2. List commands"
+echo "3. Exit"
