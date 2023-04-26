7588b8d85e5bc0ef785ba918744b0eee2e27fc5c
arquisito <112442036+arquisito@users.noreply.github.com>
2023-04-26 17:12:41 -0400

    edit ls.sh

--- scripts/ls.sh
+++ scripts/ls.sh
@@ -7,12 +7,22 @@ echo "ls stands for 'List'."
 echo "This command will print a list of the files and directories in your current directory."
 echo "This is the syntax for the ls command:"
 echo "ls [flags] [directory]"
-echo "You can print the lists of other directories using these commands:"
-echo "ls [path to directory]        lists files and directories from said directory"
-echo "ls /                          lists from the root directory"
-echo "ls ..                         lists from the parent directory before the current one"
-echo "ls ../..                      lists from the parent directory two steps up"
-echo "ls ~                          lists from the user's home directory"
+
+directoryInfo=("You can print the lists of other directories using these commands:" "ls [path to directory]        	lists files and directories from said directory" "ls /                          	lists from the root directory" "ls ..                         	lists from the parent directory before the current one" "ls ../..                      	lists from the parent directory two steps up" "ls ~                          	lists from the user's home directory")
+
+for str in ${directoryInfo[@]} ; do
+  echo "${str}"
+done
+
+# echo "You can print the lists of other directories using these commands:"
+# echo "ls [path to directory]        	lists files and directories from said directory"
+# echo "ls /                          	lists from the root directory"
+# echo "ls ..                         	lists from the parent directory before the current one"
+# echo "ls ../..                      	lists from the parent directory two steps up"
+# echo "ls ~                          	lists from the user's home directory"
+echo "Here are some useful flags for the ls command, which can be used in tandem with the previous directory shortcuts to work beyond your present working directory:"
+echo "ls -d */				lists only directories"
+echo "ls -R				lists all files and directories as well as their subdirectories down to the last file"
 
 skip=false
 menu=false
