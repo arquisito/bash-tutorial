7d09f37e35389a6b27088267fc21f20bc5ca2df1
arquisito <112442036+arquisito@users.noreply.github.com>
2023-04-27 00:52:46 -0400

    create sudo.sh

---
+++ scripts/sudo.sh
@@ -0,0 +1,10 @@
+clear
+echo -en "\n"
+echo "4. sudo"
+
+echo -en "\n"
+echo "sudo is short for 'Super Do'."
+#echo "This command will print a list of the files and directories in your current directory."
+#echo -en "\n"
+#echo "This is the syntax for the ls command:"
+#echo "  ls [flags] [directory]"
