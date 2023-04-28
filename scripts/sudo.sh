58da187681be8409b0645880923bb9cc2a7b8d7d
root <root@final-project.c.teehee-375116.internal>
2023-04-28 21:40:07 +0000

    modify permissions

--- scripts/sudo.sh
+++ scripts/sudo.sh
@@ -5,14 +5,14 @@ echo "4. sudo"
 echo -en "\n"
 echo "sudo stands for 'Super User Do'."
 echo -e "\nThis command is actually less of its own command, and more of a prefix to other commands."
-echo -e "The sudo command allows you to run another command with elevated privileges--basically, it runs the command as if you are the root user.\nSome bash commands will not execute properly (or at all) if the user running the command is not the root user. You can switch to the root user using different commands, but this runs the risk of you forgetting to switch back to your user. This could lead to you performing an irreversible action because you are doing them as root (such as accidentally erasing things permanently that you did not mean to).\nWith sudo, you can execute commands with elevated privileges without having to worry about switching between users." | fold -w100 -s
+echo -e "The sudo command allows you to run another command with elevated privileges--basically, it runs the command as if you are the root user.\n\nSome bash commands will not execute properly (or at all) if the user running the command is not the root user. You can switch to the root user using different commands, but this runs the risk of you forgetting to switch back to your user. This could lead to you performing an irreversible action because you are doing them as root (such as accidentally erasing things permanently that you did not mean to).\n\nWith sudo, you can execute commands with elevated privileges without having to worry about switching between users." | fold -w100 -s
 echo -en "\n"
 echo "The syntax for the sudo command is as follows:"
 echo -e "\tsudo [options] [command]"
 echo -en "\n"
-echo "sudo has a few flags that can be used before a command, but most of them have to do with switching users (which is unnecessary for any tutorials here), so we will only focus on one:" | fold -w100 -s
+echo -e "sudo has a few flags that can be used before a command, but most of them have to do with switching users.\nThis is unnecessary for any tutorials here, so we will only focus on one:"
 echo -e "\tsudo -b [command]\t\truns the command in the background (with elevated privileges)"
-echo "This flag is useful for running commands that will output a lot of text, such as an update. It can be used for other commands as well, but keep in mind that it will not show the output for those commands." | fold -w100 -s
+echo -e "\nThis flag is useful for running commands that will output a lot of text, such as an update. It can be used for other commands as well, but keep in mind that it will not show the output for those commands.\n" | fold -w100 -s
 
 read -p "(Press enter to continue.) >" ans
 
