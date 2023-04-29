b9262993e5bea4b9259d8cbf55426538843953d2
arquisito <112442036+arquisito@users.noreply.github.com>
2023-04-29 18:59:11 -0400

    Update history.sh

--- scripts/history.sh
+++ scripts/history.sh
@@ -4,19 +4,21 @@ echo "11. history"
 
 echo -en "\n"
 echo "The history command is self explanatory; it stands for 'History'."
-echo "This command prints a numbered list of all of previously executed commands, including your execution of the history command."
+echo "This command prints a numbered list of all of previously executed commands, including your execution of the history command." | fold -w100 -s
 echo -e "\nThis goes all the way back to the first command you ever used on your current operating system, so this could potentially be a very long list." | fold -w100 -s
 echo -en "\n"
 echo "This is the syntax for the history command:"
 echo -e "\thistory [flags]"
 
 flagInfo=("Here are some useful flags and shortcuts for the history command:"
-"\thistory [#]\t\t\t\tprints the specified number of previously executed commands, including"
-"\t\t\t\t\t\t   the history command"
-"\t![event #]\t\t\t\texecutes the command with the specified event number (number assigned"
-"\t\t\t\t\t\t   to the command in history)."
-"\t![event #]:p\t\t\t\tprints the command with the specified event number before executing it"
-"\thistory -d [event #]\t\t\tremoves the command with the specified event number from the history"
+"\thistory [#]\t\t\t\tprints the specified number of previously executed commands,"
+"\t\t\t\t\t\t   including the history command"
+"\t![event #]\t\t\t\texecutes the command with the specified event number (number"
+"\t\t\t\t\t\t   assigned to the command in history)."
+"\t![event #]:p\t\t\t\tprints the command with the specified event number"
+"\t\t\t\t\t\t   executing it"
+"\thistory -d [event #]\t\t\tremoves the command with the specified event number from"
+"\t\t\t\t\t\t   the history"
 "\thistory -c\t\t\t\tfully clears the entire command history")
 
 echo -en "\n"
