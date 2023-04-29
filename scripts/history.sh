5e45b2cefd9576dbbf8edb89d2c6e2802371af15
arquisito <112442036+arquisito@users.noreply.github.com>
2023-04-29 19:06:02 -0400

    Update history.sh

--- scripts/history.sh
+++ scripts/history.sh
@@ -28,3 +28,113 @@ done
 
 echo -en "\n"
 read -p "(Press enter to continue.) >" ans
+
+skip=false
+menu=false
+keepGoing=true
+while [ ${keepGoing} = true ]; do
+	echo -en "\n"
+	echo "Enter the number of the option you would like to proceed to:"
+	echo "1. Practice problem"
+	echo "2. Skip practice / Next tutorial"
+	echo "3. Return to menu"
+	echo -en "\n"
+	read -p "> " input
+    
+	if [ "${input}" == "1" ]; then
+    
+    usr=`whoami`
+    currentDir=`pwd`
+	if [ "${currentDir}" != "/home/${usr}/bash-tutorial" ]; then
+        cd /home/${usr}/bash-tutorial
+	fi
+    # practice
+		echo -en "\n"
+		echo -e "Practice Problem:\n"
+		echo -e "Use the history command to clear your entire command history." | fold -w100 -s
+		echo -e "To show the solution, enter 'solve'. To skip this question and proceed to the next tutorial, enter 'skip'. To return to the menu, enter 'menu'.\n" | fold -w100 -s
+        	echo "If you need the list of flags, enter 'flags':"
+		problemBoolean=true
+        
+        while [ ${problemBoolean} = true ]; do
+			echo -en "\n"
+			read -p "> " answer
+		if [ "${answer}" == "sudo history -c" ] || [ "${answer}" == "history -c" ] ; then
+                	echo -en "\n"
+		        	echo "You've done it; you cleared your history!"
+		        	problemBoolean=false
+		        	keepGoing=false
+		        	skip=true
+            elif [ "${answer}" == "skip" ]; then
+				echo -en "\n"
+				echo "Moving on to the next command."
+				problemBoolean=false
+				keepGoing=false
+				skip=true
+            elif [ "${answer}" == "solve" ]; then
+				currentDir=`pwd`
+				if [ "${currentDir}" != "/home/${usr}/bash-tutorial" ]; then
+        				cd /home/${usr}/bash-tutorial
+				fi
+				echo -e "\n***"
+				echo -en "\n"
+                echo "Clearing your command history is quite simple; all you have to do is enter the following command:"
+				echo -en "\n"
+				echo "> history -c"
+				history -c
+                echo -e "\nThis simple command has cleared your entire command history, which you can check by using the history command again:" | fold -w100 -s
+				history
+				echo -e "\nAs you can see, your history has been cleared except for your new execution of history."
+				echo -e "\n***\n"
+				problemBoolean=false
+				cd ..
+				read -p "(Press enter to continue.)>" ans
+			elif [ "${answer}" == "menu" ]; then
+				echo -en "\n"
+				echo "Returning to the menu."
+				problemBoolean=false
+				keepGoing=false
+				menu=true
+            elif [ "${answer}" == "flags" ]; then
+				echo -en "\n"
+                for ((x = 0; x < ${#flagInfo[@]}; x++)) ; do
+                    echo -e "${flagInfo[$x]}"
+                done
+			else
+				${answer}
+				if [ $? -ne 0 ]; then
+					echo "Command failed. Try another input."
+					sleep 2
+				fi
+			fi
+		done
+    elif [ "${input}" == "2" ]; then
+		echo -en "\n"
+		echo "Moving on to the next command."
+		keepGoing=false
+		skip=true
+	elif [ "${input}" == "3" ]; then
+		echo -en "\n"
+		echo "Returning to the menu."
+		keepGoing=false
+		menu=true
+	else 
+		echo "You entered an invalid input. Please try again."
+	fi
+done
+
+currentDir=`pwd`
+usr=`whoami`
+if [ "${currentDir}" != "/home/${usr}/bash-tutorial" ]; then
+	cd /home/${usr}/bash-tutorial
+fi
+if [ -d /home/${usr}/bash-tutorial/practice/chmodDir ]; then
+    sudo rm -r /home/${usr}/bash-tutorial/practice/chmodDir
+fi
+if [ ${skip} = true ]; then
+	sleep 2
+	bash ./scripts/history.sh
+elif [ ${menu} = true ]; then
+	sleep 2
+	bash ./welcome.sh
+fi	
