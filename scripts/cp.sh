bb9d32f34f1555d9514fcdfa7541c2d970599342
arquisito <112442036+arquisito@users.noreply.github.com>
2023-04-28 21:03:47 -0400

    Update cp.sh

--- scripts/cp.sh
+++ scripts/cp.sh
@@ -7,9 +7,9 @@ echo "cp stands for 'Copy'."
 echo "This command will copy files, groups of files, or directories. It creates an exact image of the specified file on the disk with a different name." | fold -w100 -s
 echo -en "\n"
 echo "This is the syntax for the cp command:"
-echo -e "\tcp [flags] [source] [destination]\t\t(copies one file)"
-echo -e "\tcp [flags] [source] [directory]\t\t(copies one file)"
-echo -e "\tcp [flags] [source 1] [source 2] [source 3] [directory]\t(copies multiple files)"
+echo -e "\tcp [flags] [source] [destination]\t\t\t(copies one file)"
+echo -e "\tcp [flags] [source] [directory]\t\t\t\t\t(copies one file)"
+echo -e "\tcp [flags] [source 1] [source 2] [source 3] [directory]\t\t(copies multiple files)"
 
 
 
